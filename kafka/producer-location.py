ad75207cdfbc3fd45ded8adc256ebdb6e75e229e
irahulcse <1rahulchandra1@gmail.com>
2024-01-27 15:47:36 +0100

    change

--- kafka/producer-location.py
+++ kafka/producer-location.py
@@ -1,6 +1,9 @@
 from confluent_kafka import Producer
 import csv
 import json
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
 
 class LocationData:
     def __init__(self, x, y, t):
@@ -11,13 +14,26 @@ class LocationData:
     def serialize(self):
         return json.dumps(self.__dict__)
 
-p = Producer({'bootstrap.servers': 'localhost:9092'})
+class FileChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        p = Producer({'bootstrap.servers': 'localhost:9092'})
+        with open('../textInputSources/locationSource.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                location_data = LocationData(int(row['x']), int(row['y']), int(row['t']))
+                serialized_data = location_data.serialize()
+                print('Sending message: {}'.format(serialized_data))  # Print the data being sent
+                p.produce('location-topic', serialized_data)
+        p.flush()
 
-with open('../textInputSources/locationSource.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print(row.keys())  # Print the keys of the row
-        location_data = LocationData(int(row['x']), int(row['y']), int(row['t']))
-        p.produce('location-topic', location_data.serialize())
+observer = Observer()
+event_handler = FileChangeHandler()
+observer.schedule(event_handler, path='../textInputSources/locationSource.csv')
+observer.start()
 
-p.flush()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
