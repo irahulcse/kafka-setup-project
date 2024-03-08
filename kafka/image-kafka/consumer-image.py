d705ffd59ca93a8ee1974cdf9c7adc538c40ce54
irahulcse <1rahulchandra1@gmail.com>
2024-03-08 10:01:28 +0100

    updated imag

--- kafka/image-kafka/consumer-image.py
+++ kafka/image-kafka/consumer-image.py
@@ -1,6 +1,7 @@
-from confluent_kafka import Consumer, KafkaError
+import os
 import json
 import base64
+from confluent_kafka import Consumer, KafkaError
 
 class ImageData:
     def __init__(self, filename, content):
@@ -13,7 +14,7 @@ class ImageData:
         return cls(**params)
 
 c = Consumer({
-    'bootstrap.servers': 'localhost:9092',
+    'bootstrap.servers': '192.168.221.213:9092',
     'group.id': 'mygroup',
     'auto.offset.reset': 'earliest'
 })
@@ -21,19 +22,61 @@ c = Consumer({
 c.subscribe(['image-topic'])
 
 while True:
-    msg = c.poll(1.0)
+    msg = c.poll(0.1)  # Reduced the timeout to make polling more frequent
     if msg is None:
         continue
     if msg.error():
         print("Consumer error: {}".format(msg.error()))
         continue
 
-    if msg.topic() == 'image-topic':
-        data = ImageData.deserialize(msg.value().decode('utf-8'))
-        # Decode the base64 content back into bytes
-        content = base64.b64decode(data.content)
-        # Now you can write the content to a file, or process it directly in your application
+    try:
+        image_data = ImageData.deserialize(msg.value().decode('utf-8'))
+        print('Received message: {}'.format(image_data.__dict__))
 
-    print(f'Received message on topic {msg.topic()}: {data.filename}')
+        # Save the image to a file
+        with open(f'../../getImage/{image_data.filename}', 'wb') as f:
+            f.write(base64.b64decode(image_data.content))
+    except json.decoder.JSONDecodeError:
+        print(f"Unable to parse message: {msg.value()}")
 
 c.close()
+
+# from confluent_kafka import Consumer, KafkaError
+# import json
+# import base64
+
+# class ImageData:
+#     def __init__(self, filename, content):
+#         self.filename = filename
+#         self.content = content  # Base64-encoded image content
+
+#     @classmethod
+#     def deserialize(cls, data):
+#         params = json.loads(data)
+#         return cls(**params)
+
+# c = Consumer({
+#     'bootstrap.servers': 'localhost:9092',
+#     'group.id': 'mygroup',
+#     'auto.offset.reset': 'earliest'
+# })
+
+# c.subscribe(['image-topic'])
+
+# while True:
+#     msg = c.poll(1.0)
+#     if msg is None:
+#         continue
+#     if msg.error():
+#         print("Consumer error: {}".format(msg.error()))
+#         continue
+
+#     if msg.topic() == 'image-topic':
+#         data = ImageData.deserialize(msg.value().decode('utf-8'))
+#         # Decode the base64 content back into bytes
+#         content = base64.b64decode(data.content)
+#         # Now you can write the content to a file, or process it directly in your application
+
+#     print(f'Received message on topic {msg.topic()}: {data.filename}')
+
+# c.close()
